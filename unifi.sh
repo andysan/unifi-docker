049d3657d3b081966c87a07800ae68a3dcc0670f
Andreas Sandberg <andreas@sandberg.pp.se>
2017-04-30 19:28:59 +0100

    Cleanup run script

--- unifi.sh
+++ unifi.sh
@@ -1,20 +1,20 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+set -e
 
 # vars similar to those found in unifi.init
-JSVC=$(command -v jsvc)
-PIDFILE=/var/run/unifi/unifi.pid
+JSVC=/usr/bin/jsvc
+PIDFILE=${RUNDIR}/unifi.pid
 JVM_OPTS="
   -Dunifi.datadir=${DATADIR}
   -Dunifi.rundir=${RUNDIR}
   -Dunifi.logdir=${LOGDIR}
   -Djava.awt.headless=true
-  -Dfile.encoding=UTF-8"
-if [ ! -z "${JVM_MAX_HEAP_SIZE}" ]; then
-	JVM_OPTS="${JVM_OPTS} -Xmx${JVM_MAX_HEAP_SIZE}"
-fi
-if [ ! -z "${JVM_INIT_HEAP_SIZE}" ]; then
-  JVM_OPTS="${JVM_OPTS} -Xms${JVM_INIT_HEAP_SIZE}"
-fi
+  -Dfile.encoding=UTF-8
+  ${JVM_MAX_HEAP_SIZE:+-Xmx${JVM_MAX_HEAP_SIZE}}
+  ${JVM_INIT_HEAP_SIZE:+-Xms${JVM_INIT_HEAP_SIZE}}
+"
+
 JSVC_OPTS="
   -home ${JAVA_HOME}
   -classpath /usr/share/java/commons-daemon.jar:${BASEDIR}/lib/ace.jar
@@ -23,20 +23,25 @@ JSVC_OPTS="
   -outfile ${LOGDIR}/unifi.out.log
   -errfile ${LOGDIR}/unifi.err.log
   ${JVM_OPTS}"
-# One issue might be no cron and lograte, causing the log volume to become bloated over time! Consider `-keepstdin` and `-errfile &2` options for JSVC.
+
+# One issue might be no cron and lograte, causing the log volume to
+# become bloated over time! Consider `-keepstdin` and `-errfile &2`
+# options for JSVC.
 MAINCLASS='com.ubnt.ace.Launcher'
 
 # trap SIGTERM (or SIGINT or SIGHUP) and send `-stop`
-trap "echo 'Stopping unifi controller service (TERM signal caught).'; ${JSVC} -nodetach -pidfile ${PIDFILE} -stop ${MAINCLASS} stop; exit 0" 1 2 15
+stop_unifi()
+{
+    echo 'Stopping unifi controller service (TERM signal caught).' >&2
+    ${JSVC} -nodetach -pidfile ${PIDFILE} -stop ${MAINCLASS} stop
+}
 
-# Cleaning /var/run/unifi/* See issue #26, Docker takes care of exlusivity in the container anyway.
 rm -f /var/run/unifi/unifi.pid
+trap stop_unifi 1 2 15
 
 # keep attached to shell so we can wait on it
 echo 'Starting unifi controller service.'
-${JSVC} -nodetach ${JSVC_OPTS} ${MAINCLASS} start &
-
-wait
-
-echo "WARN: unifi service process ended without being singaled? Check for errors in ${LOGDIR}." >&2
-exit 1
+if ! ${JSVC} -nodetach ${JSVC_OPTS} ${MAINCLASS} start; then
+    echo "Error: Unexpected error code from daemon: $?" >&2
+    exit 2
+fi
